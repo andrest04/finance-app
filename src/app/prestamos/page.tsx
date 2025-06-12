c9859c1567c72aff05a0fcef66bc12dab37fed9b
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2025-06-11 20:12:52 -0500

    feat: add PrestamosVariablesPage component for loan variables calculation

---
+++ src/app/prestamos/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import CalculadoraPrestamosVariables from "@/components/ui/CalculadoraPrestamosVariables";
+
+export default function PrestamosVariablesPage() {
+  return <CalculadoraPrestamosVariables />;
+}
