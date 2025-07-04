2858bb533e1e54555a3915291a411f9bfde1ff11
Andrés Torres <131729113+andrest04@users.noreply.github.com>
2025-07-04 10:16:16 -0500

    feat(lib): add centralized index exports

---
+++ src/lib/firebase/index.ts
@@ -0,0 +1,5 @@
+export * from './adminUtils';
+export * from './errorMessages';
+export * from './firebase';
+export * from './useCurrentUser';
+export * from './userUtils';
---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export * from './utils';
+
+export * from './bono';
+export * from './calculations';
+export * from './firebase';
